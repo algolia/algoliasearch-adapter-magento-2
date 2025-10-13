cb6a6bd100a387b7469f06907f883bef74a19bd6
Eric Wright <eric.wright@algolia.com>
2025-10-13 10:32:13 -0400

    MAGE-1317 Add adapter engine config

---
+++ Model/ResourceModel/Engine.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Algolia\SearchAdapter\Model\ResourceModel;
+
+use Magento\CatalogSearch\Model\ResourceModel\EngineInterface;
+use Magento\Eav\Model\Entity\Attribute;
+
+class Engine implements EngineInterface
+{
+
+    public function getAllowedVisibility(): array
+    {
+        // TODO: Implement getAllowedVisibility() method.
+        return [];
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function allowAdvancedIndex(): bool
+    {
+        return false;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function processAttributeValue($attribute, $value): mixed
+    {
+        // TODO: Implement processAttributeValue() method.
+        return $value;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function prepareEntityIndex($index, $separator = ' '): array
+    {
+        // TODO: Implement prepareEntityIndex() method.
+        return $index;
+    }
+}
