977b05075271ed21cbe37e49e2ca31592f0cd956
Eric Wright <eric.wright@algolia.com>
2025-10-16 06:24:08 -0400

    MAGE-1318 Disable indexer

--- Model/ResourceModel/Engine.php
+++ Model/ResourceModel/Engine.php
@@ -39,4 +39,10 @@ class Engine implements EngineInterface
         // TODO: Implement prepareEntityIndex() method.
         return $index;
     }
+
+    /** Not part of interface but called by \Magento\CatalogSearch\Model\ResourceModel\EngineProvider */
+    public function isAvailable(): bool
+    {
+        return false; // preventing indexing for now - but ugly error
+    }
 }
