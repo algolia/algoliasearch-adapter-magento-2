fbf79c8b0e2c2eb68d0ed83cf007dd39a0baff74
Eric Wright <eric.wright@algolia.com>
2026-01-20 14:13:26 -0500

    MAGE-1325 Link backend rendering to search engine selection

---
+++ Model/Config/Comment/BackendRendering.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Algolia\SearchAdapter\Model\Config\Comment;
+
+use Algolia\AlgoliaSearch\Model\Config\AbstractConfigComment;
+
+class BackendRendering extends AbstractConfigComment
+{
+    public function getCommentText($elementString): string
+    {
+        $link = $this->getConfigLink(
+            'catalog',
+            'catalog_search-head'
+        );
+
+        return <<<COMMENT
+            Backend rendering can be enabled for all users, or selectively for specific user agents (for example, search engine bots),
+            while regular shoppers continue to use InstantSearch.
+            <br><br>
+            <strong>Important:</strong> Enabling backend rendering may result in additional Algolia API calls.
+            It is strongly recommended to enable Magento’s Full Page Cache to avoid duplicate requests and increased search usage.
+            <br><br>
+            <div class="algolia_dashboard_warning">
+                <p>Backend rendering requires the Magento search engine to be set to <strong>Algolia Backend Search</strong>.</p>
+                <p>You can configure this in: <a href="$link">Stores &gt; Configuration &gt; Catalog &gt; Catalog &gt; Catalog Search</a></p>
+            </div>
+        COMMENT;
+    }
+}
